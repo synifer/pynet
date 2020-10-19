f8a1248a0241515f06e69f5a15a95934798beb49
synifer <36164494+synifer@users.noreply.github.com>
2020-10-19 08:01:32 +0300

    Update task_4_5.py

--- exercises/04_data_structures/task_4_5.py
+++ exercises/04_data_structures/task_4_5.py
@@ -13,3 +13,23 @@
 
 command1 = "switchport trunk allowed vlan 1,2,3,5,8"
 command2 = "switchport trunk allowed vlan 1,3,8,9"
+
+
+Решение:
+  
+  Для решения данной задачи использую метод split() для разбития строк на части с
+  использованием разделителя ','. Каждый список записываю в свою переменную. С помощью split 
+  commands[-1].split(',') получаю список vlan. 
+  С помощью метода set() преобразовую списки в множества и применяю метод intersection()
+  для получения списка пересекающихся vlan
+  
+  
+ command1 = "switchport trunk allowed vlan 1,2,3,5,8"
+ command2 = "switchport trunk allowed vlan 1,3,8,9"
+ list_vlan1 = command1.split()
+ vlans1 = list_vlan1[-1].split(',')
+ list_vlan2 = command2.split()
+ vlans2 = list_vlan2[-1].split(',')
+ vlans1 = set(vlans1)
+ vlans2 = set(vlans2)
+ vlans1.intersection(vlans2)
