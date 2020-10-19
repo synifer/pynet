6fd419a226e17d7dcc32bb277145d166cd0a0088
synifer <36164494+synifer@users.noreply.github.com>
2020-10-19 15:12:24 +0300

    Update task_4_6.py

--- exercises/04_data_structures/task_4_6.py
+++ exercises/04_data_structures/task_4_6.py
@@ -14,3 +14,24 @@ Outbound Interface    FastEthernet0/0
 """
 
 ospf_route = "      10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
+
+
+Решение:
+  
+  
+  
+  spf_route = """
+    ...: Prefix                10.0.24.0/24
+    ...: AD/Metric             110/41
+    ...: Next-Hop              10.0.13.3
+    ...: Last update           3d18h
+    ...: Outbound Interface    FastEthernet0/0
+    ...: """
+  
+  print(spf_route)
+
+Prefix                10.0.24.0/24
+AD/Metric             110/41
+Next-Hop              10.0.13.3
+Last update           3d18h
+Outbound Interface    FastEthernet0/0
