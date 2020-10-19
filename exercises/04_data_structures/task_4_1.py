b860d6da28f0f4cc79cbb37eccbb6d5206bb7d5c
synifer <36164494+synifer@users.noreply.github.com>
2020-10-19 06:55:40 +0300

    Update task_4_1.py

--- exercises/04_data_structures/task_4_1.py
+++ exercises/04_data_structures/task_4_1.py
@@ -10,3 +10,19 @@
 """
 
 nat = "ip nat inside source list ACL interface FastEthernet0/1 overload"
+
+Для выполнения данного задания я использую метод Replace()
+Метод позволяет осуществить замену последовательности символов в строке на другую последовательность
+После применения метода Replace() присваиваю новое значение переменной.
+
+Ответ:
+  
+   string1 = 'nat = "ip nat inside source list ACL interface FastEthernet0/1 overload"'
+   string1.replace('Fast', 'Gigabit')
+   string1 = string1.replace('Fast', 'Gigabit')
+    
+
+  
+    
+  
+ 
